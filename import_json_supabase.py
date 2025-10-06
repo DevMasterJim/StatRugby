126b0d8a0955889ab1ed05e6ceae986a0afce4b2
DevMasterJim <domoarno@gmail.com>
2025-10-06 16:20:15 +0200

    Update import_json_supabase.py

--- import_json_supabase.py
+++ import_json_supabase.py
@@ -1,6 +1,6 @@
-import json
 import streamlit as st
-from supabase import create_client, Client
+import requests
+import json
 from datetime import datetime
 
 st.title("Import JSON Match → Supabase")
